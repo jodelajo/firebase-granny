e412ef8374d53d96b517ee86f8e6e0ad83e91707
jodelajo <jodelajo@gmail.com>
2021-04-22 16:41:15 +0200

    t/m deel 1 workshop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import './App.css';
-import React from "react";
+import React, {useState} from "react";
 import './App.css'
+import app from "./modules/firebase";
+// const log = (...messages ) => {
+//     if( process.env.debug) console.log(...messages)
+// }
 
 function App() {
-  return <main>
-    <div id="login-form">
-      <h1>Register</h1>
-      <input type="email" placeholder="email"/>
-      <input type="password" placeholder="password"/>
-    </div>
-  </main>
+    const [userIntent, setUserIntent] = useState('Register')
+    const [ appUser, setUser] = useState()
+
+    const onSubmit = async event => {
+
+        event.preventDefault()
+        const [email, password] = event.target
+
+        if(userIntent === 'Register') {
+            const response = await app.auth().createUserWithEmailAndPassword(email.value, password.value)
+            console.log('authentication response', response)
+            setUser(response.user)
+        } else {
+            const response = await app.auth().signInWithEmailAndPassword(email.value, password.value)
+
+            console.log('authentication response', response)
+            setUser(response.user)
+        }
+    }
+    return <main>
+        { !appUser && <form id="login-form" onSubmit={onSubmit}>
+            <h1>{userIntent}</h1>
+            {appUser && <h2>{appUser.email}</h2>}
+
+            <input type="email" placeholder="email"/>
+            <input type="password" placeholder="password"/>
+            <input type="submit" value={userIntent}/>
+            <button onClick={() => setUserIntent(userIntent === 'Register' ? 'Login' : 'Register')}>
+                {userIntent === 'Register' ? 'Login' : 'Register'} instead
+            </button>
+        </form>}
+
+        { appUser && <h1> Welcome { appUser.email } </h1>}
+    </main>
 }
 
 export default App;
---
+++ src/modules/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyDWfYO-51-52aK-xCKxQ8iBjWHaIiamWME",
+    authDomain: "granny-test-ebfd5.firebaseapp.com",
+    projectId: "granny-test-ebfd5",
+    storageBucket: "granny-test-ebfd5.appspot.com",
+    messagingSenderId: "541335387200",
+    appId: "1:541335387200:web:d76635dbc95d35817c33a5",
+});
+export default app
